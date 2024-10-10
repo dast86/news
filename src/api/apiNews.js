ccff19a4cbb635d4d2c85896fcfe894af9a4af64
Alex Popov <popov_a_a_86@mail.ru>
2024-10-10 15:52:29 +0500

    categories

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -4,13 +4,30 @@ const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
 
-export const getNews = async (page_number=1, page_size=10) => {
+
+
+export const getCategories = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}available/categories`, {
+            params: {
+                apiKey: API_KEY,
+            }
+        })
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const getNews = async ({page_number=1, page_size=10, category}) => {
     try {
         const response = await axios.get(`${BASE_URL}search`, {
             params: {
                 apiKey: API_KEY,
                 page_size,
                 page_number,
+                category,
             }
         })
         return response.data
